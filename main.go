cb04359af0c4cc9419c28844ead65eea87432a4f
Timothy Loftus <tloftus@protonmail.com>
2025-01-06 09:44:06 -0600

    Substantial update
    Can now backup dotfiles in the home directory.
    Now backs up directories.
    
    Added backup flag

--- main.go
+++ main.go
@@ -7,29 +7,33 @@ import (
 
     "github.com/spf13/cobra"
     "github.com/spf13/viper"
-    "github.com/n3s0/dotfilesync/dfs"
 )
 
-type DotFileConfig struct {
-    SyncBaseDir string
-    XdgBaseDir string
-    SyncDirs []string
-    DotFiles []string
-    DotFilesIgnore []string
+type Config struct {
+	Sync struct {
+		SyncDir string `mapstructure:"sync_dir"`
+	} `mapstructure:"sync"`
+	Dotfiles struct {
+		DirPaths  []string `mapstructure:"dir_paths"`
+		FilePaths []string `mapstructure:"file_paths"`
+	} `mapstructure:"dotfiles"`
 }
 
 var cfgFile string
 var syncBaseDir string
 var dotFilePaths []string
+var verbose bool
 var sync bool
+var backup bool
+var push bool
 
 var versionCmd = &cobra.Command{
-  Use:   "version",
-  Short: "Print the version number of dotfilesync",
-  Long:  `Print the version number of dotfilesync`,
-  Run: func(cmd *cobra.Command, args []string) {
-    fmt.Println("dotfilesync v0.1")
-  },
+    Use:   "version",
+    Short: "Print the version number of dotfilesync",
+    Long:  `Print the version number of dotfilesync`,
+    Run: func(cmd *cobra.Command, args []string) {
+        fmt.Println("dotfilesync v0.1")
+    },
 }
 
 var rootCmd = &cobra.Command{
@@ -37,71 +41,64 @@ var rootCmd = &cobra.Command{
     Short: "A command-line dotfile syncing tool.",
     Long: `A command-line dotfile syncing tool.`,
     Run: func(cmd *cobra.Command, args []string) {
-        var config DotFileConfig
+        var config Config
 
         err := viper.Unmarshal(&config)
         if err != nil {
-            fmt.Println("Error:", err)
+            fmt.Printf("Error: %s\n", err)
             os.Exit(1)
         }
-        
+
         home, err := os.UserHomeDir()
-        cobra.CheckErr(err)
-
-        syncBaseDir = fmt.Sprintf("%s/%s", home, config.SyncBaseDir)
-        err = dfs.CreateDirIfNotExist(syncBaseDir)
         if err != nil {
-            fmt.Println("Error:", err)
+            fmt.Printf("Error: %s\n", err)
             os.Exit(1)
         }
 
-        xdgBaseDir := fmt.Sprintf("%s/%s/%s", home, config.SyncBaseDir, config.XdgBaseDir)
-        err = dfs.CreateDirIfNotExist(xdgBaseDir)
+        // Default directory for putting the sync directory is the root of home.
+        syncDir := fmt.Sprintf("%s/%s", home, config.Sync.SyncDir)
+        err = CreateDirIfNotExist(syncDir)
         if err != nil {
             fmt.Println("Error", err)
             os.Exit(1)
         }
 
+        dotDirPaths := config.Dotfiles.DirPaths
+        dotFilePaths = config.Dotfiles.FilePaths
 
-        if sync {
-            for _, dir := range config.SyncDirs {
-                syncPath := fmt.Sprintf("%s%s", syncBaseDir, dir)
-                fmt.Println("syncpath", syncPath)
+        if backup {
+            fmt.Printf("Backing up directories\n")
+            
+            for _, dir := range dotDirPaths {
+                syncDirPath := fmt.Sprintf("%s/%s", home, dir)
+                dotDirPath := fmt.Sprintf("%s/%s", syncDir, dir)
 
-                err := dfs.CreateDirIfNotExist(syncPath)
+                
+                err := CopyDir(syncDirPath, dotDirPath)
                 if err != nil {
-                    fmt.Println("Error:", err)
-                    os.Exit(1)
+                    fmt.Printf("Error: %s\n", err)
                 }
-            }
-
-            dotFilePaths = config.DotFiles
-
+            } 
+            
+            fmt.Printf("Backing up dotfiles\n")
+            
             for _, file := range dotFilePaths {
                 syncFilePath := fmt.Sprintf("%s/%s", home, file)
-                dotFilePath := fmt.Sprintf("%s/%s", syncBaseDir, file)
-                fmt.Println("syncpath2", syncFilePath)
-                fmt.Println("dotfilepath", dotFilePath)
+                dotFilePath := fmt.Sprintf("%s/%s", syncDir, file)
 
-                needsSynced, err := dfs.CompareFiles(syncFilePath, dotFilePath)
+                err := CopyFile(syncFilePath, dotFilePath)
                 if err != nil {
-                    fmt.Println("Error:", err)
-                    os.Exit(1)
-                }
-
-                switch needsSynced {
-                case "dotfile":
-                    fmt.Printf("Copying %s -> %s\n", dotFilePath, syncFilePath)
-                    dfs.CopyFile(dotFilePath, syncFilePath)
-                case "syncfile":
-                    fmt.Printf("Copying %s -> %s\n", syncFilePath, dotFilePath)
-                    dfs.CopyFile(syncFilePath, dotFilePath)
-                case "synced":
-                    fmt.Println("All files are synced")
-                default:
-                    fmt.Println("All files are synced")
+                    fmt.Printf("Error: %s\n", err)
                 }
             }
+        }
+
+        if sync {
+            fmt.Println("I will eventually sync the files to the home directory")
+        }
+
+        if push {
+            fmt.Println("I will eventually commit and push the dotfiles git repo")
         }
     },
 }
@@ -116,18 +113,26 @@ func Execute() {
 func init() {
     cobra.OnInitialize(initConfig)
 
-    rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.config/dotfilesync/config.yaml)")
-    rootCmd.PersistentFlags().BoolVarP(&sync, "sync", "s", false, "sync dotfiles to dotfile directory")
+    rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "Config file (default is $HOME/.config/dotfilesync/config.yaml)")
+    rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "", false, "Show verbose output")
+    rootCmd.PersistentFlags().BoolVarP(&sync, "sync", "s", false, "Sync dotfiles to the home directory. Will sync to dotfiles directory too.")
+    rootCmd.PersistentFlags().BoolVarP(&backup, "backup", "b", false, "Backup dotfiles to sync directory")
+    rootCmd.PersistentFlags().BoolVarP(&push, "push", "p", false, "Adding, commit, and push files to git repo")
 }
 
 func initConfig() {
+    // Base directory for the configuration is $HOME/.config/dotfilesync. Config
+    // flag will update this.
     var configDir string = ".config/dotfilesync"
 
     if cfgFile != "" {
         viper.SetConfigFile(cfgFile)
     } else {
         home, err := os.UserHomeDir()
-        cobra.CheckErr(err)
+        if err != nil {
+            fmt.Printf("Error: %s", err)
+            os.Exit(1)
+        }
 
         defaultConfigPath := fmt.Sprintf("%s/%s", home, configDir)
 
@@ -140,6 +145,8 @@ func initConfig() {
         fmt.Println("Cannot read config:", err)
         os.Exit(1)
     }
+    
+    fmt.Printf("Config file loaded: %s\n", viper.ConfigFileUsed())
 }
 
 /*
@@ -149,7 +156,7 @@ sync path. But, this can also be used to copy regular text files pretty anywhere
 func CopyFile(srcPath, dstPath string) (e error) {
     srcFile, err := os.Stat(srcPath)
     if err != nil {
-        return fmt.Errorf("Error: %v", err)
+        return fmt.Errorf("Error: %v\n", err)
     }
 
     if !srcFile.Mode().IsRegular() {
@@ -158,34 +165,34 @@ func CopyFile(srcPath, dstPath string) (e error) {
 
     src, err := os.Open(srcPath)
     if err != nil {
-        return fmt.Errorf("Error: %v", err)
+        return fmt.Errorf("Error: %v\n", err)
     }
 
     defer src.Close()
 
     dst, err := os.Create(dstPath)
     if err != nil {
-        return fmt.Errorf("Error: %v", err)
+        return fmt.Errorf("Error: %v\n", err)
     }
 
     defer dst.Close()
 
+    if verbose {
+        fmt.Printf("Copying %s -> %s\n", src.Name(), dst.Name())
+    }
+    
     _, err = io.Copy(dst, src)
     if err != nil {
-        return fmt.Errorf("Error: %v", err)
+        return fmt.Errorf("Error: %s\n", err)
     }
 
     return nil
 }
 
 func CopyDir(srcPath, dstPath string) (e error) {
-    srcDir, err := os.Stat(srcPath)
+    err := os.MkdirAll(dstPath, 0755)
     if err != nil {
-        return fmt.Errorf("Error: %v", err)
-    }
-
-    if !srcDir.IsDir() {
-        return fmt.Errorf("Error: Path (%s) does not point to directory\n", srcDir)
+        return fmt.Errorf("Error: %s", err)
     }
 
     files, err := os.ReadDir(srcPath)
@@ -193,7 +200,32 @@ func CopyDir(srcPath, dstPath string) (e error) {
         return fmt.Errorf("Error: %v", err)
     }
 
-    fmt.Println(files)
+    for _, file := range files {
+        src := fmt.Sprintf("%s/%s", srcPath, file.Name())
+        dst := fmt.Sprintf("%s/%s", dstPath, file.Name())
+
+        if verbose {
+            fmt.Printf("Copying %s -> %s\n", src, dst)
+        }
+
+        if file.IsDir() {
+            if file.Name() == ".git" {
+                if verbose {
+                    fmt.Println("Skipping git directory")
+                }
+            } else {
+                err = CopyDir(src, dst)
+                if err != nil {
+                    return fmt.Errorf("Error: %s\n", err)
+                }
+            }
+        } else {
+            err = CopyFile(src, dst)
+            if err != nil {
+                return fmt.Errorf("Error: %s\n", err)
+            }
+        }
+    }
 
     return nil
 }
@@ -205,6 +237,7 @@ for copying files.
 */
 func CreateDirIfNotExist(path string) (e error) {
     if _, err := os.Stat(path); os.IsNotExist(err) {
+        fmt.Printf("Directory path %s does not exist. Creating it.\n", path)
         err := os.Mkdir(path, 0755)
         if err != nil {
             return fmt.Errorf("Error: %v", err)
@@ -251,6 +284,48 @@ func CompareFiles(syncPath, dotFilePath string) (sync string, e error) {
     } else {
         return synced, nil
     }
+}
+
+func CompareDirs(syncPath, dotFilePath string) (sync string, e error) {
+    const (
+        dotFile string = "dotfile"
+        syncFile string = "syncfile"
+        synced string = "synced"
+    )
+
+    syncFileInfo, syncErr := os.Stat(syncPath)
+    if syncErr != nil {
+        return "", fmt.Errorf("Error: %v", syncErr)
+    }
+
+    dotFileInfo, dotErr := os.Stat(dotFilePath)
+    if dotErr != nil {
+        return "", fmt.Errorf("Error: %v", dotErr)
+    }
+
+    if syncFileInfo.ModTime().Before(dotFileInfo.ModTime()) {
+        return syncFile, nil
+    } else if dotFileInfo.ModTime().Before(syncFileInfo.ModTime()) {
+        if os.IsNotExist(syncErr) {
+            return syncFile, nil
+        }
+        return dotFile, nil
+    } else {
+        return synced, nil
+    }
+}
+
+func GitAdd(repoPath string) (e error) {
+
+    return nil
+}
+
+func GitCommit(repoPath string) (e error) {
+    return nil
+}
+
+func GitPush(repoPath string) (e error) {
+    return nil
 }
 
 func main() {
